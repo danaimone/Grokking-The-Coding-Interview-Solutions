ce392fa4b1798d16547d4480f4920f6a1337dcdb
Dan Aimone <github@danaimone.com>
2021-08-13 16:11:49 -0700

    added binary_tree_path_sum.py

---
+++ Tree Depth First Search/binary_tree_path_sum.py
@@ -0,0 +1,30 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def has_path(root, sum):
+    if root is None:
+        return False
+
+    if root.val == sum and root.left is None and root.right is None:
+        return True
+
+    new_sum = sum - root.val
+    return has_path(root.left, new_sum) or has_path(root.right, new_sum)
+
+
+
+if __name__ == "__main__":
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Tree has path: " + str(has_path(root, 23)))
+    print("Tree has path: " + str(has_path(root, 16)))
+
+
